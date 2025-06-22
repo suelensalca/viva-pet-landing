afa56c6c6d396f5421908084ce312f693e318bd3
Suelen <suelen.salca@ringa.com.br>
2025-06-22 17:17:22 -0300

    Add hotel component and images

--- app/components/hotel/index.tsx
+++ app/components/hotel/index.tsx
@@ -2,34 +2,34 @@
 
 import { Box, Grid } from '@mui/material';
 import { SecImage, SectionWithWave, StyledImage, TopSectionWithWave } from './styles';
-import daycare1 from '../../assets/services/daycare1.jpeg';
-import daycare2 from '../../assets/services/daycare2.webp';
+import hotel1 from '../../assets/services/hotel1.jpeg';
+import hotel2 from '../../assets/services/hotel2.png';
 import { BoldTitle, GridContainer, Text } from '../UI';
 import { Constants } from './constants';
 
-export default function Daycare() {
+export default function Hotel() {
   return (
-    <Box id="creche" mt={10}>
+    <Box id="hospedagem" mt={10}>
       <TopSectionWithWave />
       <Box
         px={{ xs: 2, md: 5 }}
         py={2}
-        bgcolor="primary.main"
+        bgcolor="primary.dark"
         display="flex"
         justifyContent="center">
         <GridContainer container spacing={{ xs: 0, lg: 10 }}>
+          <Grid size={{ xs: 12, lg: 6 }} display="flex" justifyContent="center" alignItems="center">
+            <Box position="relative" height={{ xs: 300, sm: 380, md: 420 }} width={320}>
+              <StyledImage src={hotel2} alt="Anita com Beta e Carbono" fill />
+            </Box>
+            <SecImage>
+              <StyledImage src={hotel1} alt="Cachorro Carbono" fill />
+            </SecImage>
+          </Grid>
           <Grid size={{ xs: 12, lg: 6 }} textAlign="center" alignContent="center">
             <BoldTitle>{Constants.title}</BoldTitle>
             <Text mb={2}>{Constants.firstPar}</Text>
             <Text mb={2}>{Constants.secondPar}</Text>
-          </Grid>
-          <Grid size={{ xs: 12, lg: 6 }} display="flex" justifyContent="center" alignItems="center">
-            <Box position="relative" height={{ xs: 300, sm: 380, md: 420 }} width={320}>
-              <StyledImage src={daycare1} alt="Anita com Beta e Carbono" fill />
-            </Box>
-            <SecImage>
-              <StyledImage src={daycare2} alt="Cachorro Carbono" fill />
-            </SecImage>
           </Grid>
         </GridContainer>
       </Box>
