e3982a3375998c414048d3af7ed9cc3337479733
Suelen <suelen.salca@ringa.com.br>
2025-06-22 18:03:07 -0300

    Add walk component and images

--- app/components/walk/index.tsx
+++ app/components/walk/index.tsx
@@ -2,34 +2,34 @@
 
 import { Box, Grid } from '@mui/material';
 import { SecImage, SectionWithWave, StyledImage, TopSectionWithWave } from './styles';
-import sitter1 from '../../assets/services/sitter1.jpeg';
-import sitter2 from '../../assets/services/sitter2.jpeg';
+import walk1 from '../../assets/services/walk1.jpeg';
+import walk2 from '../../assets/services/walk2.jpeg';
 import { BoldTitle, GridContainer, Text } from '../UI';
 import { Constants } from './constants';
 
-export default function Sitter() {
+export default function Walk() {
   return (
-    <Box id="sitter" mt={10}>
+    <Box id="passeio" mt={10}>
       <TopSectionWithWave />
       <Box
         px={{ xs: 2, md: 5 }}
         py={2}
-        bgcolor="secondary.dark"
+        bgcolor="primary.main"
         display="flex"
         justifyContent="center">
         <GridContainer container spacing={{ xs: 0, lg: 10 }}>
+          <Grid size={{ xs: 12, lg: 6 }} display="flex" justifyContent="center" alignItems="center">
+            <Box position="relative" height={{ xs: 300, sm: 380, md: 420 }} width={320}>
+              <StyledImage src={walk1} alt="Anita com Beta e Carbono" fill />
+            </Box>
+            <SecImage>
+              <StyledImage src={walk2} alt="Cachorro" fill />
+            </SecImage>
+          </Grid>
           <Grid size={{ xs: 12, lg: 6 }} textAlign="center" alignContent="center">
             <BoldTitle>{Constants.title}</BoldTitle>
             <Text mb={2}>{Constants.firstPar}</Text>
             <Text mb={2}>{Constants.secondPar}</Text>
-          </Grid>
-          <Grid size={{ xs: 12, lg: 6 }} display="flex" justifyContent="center" alignItems="center">
-            <Box position="relative" height={{ xs: 300, sm: 380, md: 420 }} width={320}>
-              <StyledImage src={sitter1} alt="Gato" fill />
-            </Box>
-            <SecImage>
-              <StyledImage src={sitter2} alt="Hamster" fill />
-            </SecImage>
           </Grid>
         </GridContainer>
       </Box>
